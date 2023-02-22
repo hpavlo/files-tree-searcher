f695d4e0e1d78ad7e3cbccd032d4bc1269227f60
Pavlo Gook <67195687+pavlogook@users.noreply.github.com>
2023-02-23 01:01:57 +0200

    first task implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,48 @@
 package org.example;
 
+import java.io.File;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length < 3) {
+            System.out.println("Usage: java FileSearch <rootPath> <depth> <mask>");
+            return;
+        }
+
+        String rootPath = args[0];
+        int depth = Integer.parseInt(args[1]);
+        String mask = args[2];
+
+        Stack<File> stack = new Stack<>();
+        stack.push(new File(rootPath));
+
+        while (!stack.isEmpty()) {
+            File current = stack.pop();
+            int currentDepth = getDepth(new File(rootPath), current);
+            if (current.isDirectory() && currentDepth < depth) {
+                File[] files = current.listFiles();
+                if (files != null) {
+                    for (File file : files) {
+                        if (file.isDirectory()) {
+                            stack.push(file);
+                        } else {
+                            if (file.getName().contains(mask)) {
+                                System.out.println(file.getAbsolutePath());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private static int getDepth(File root, File dir) {
+        int depth = 0;
+        while (!dir.equals(root)) {
+            dir = dir.getParentFile();
+            depth++;
+        }
+        return depth;
     }
 }
